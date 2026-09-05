5ade3f1defc0d3b8b7d74177531ee3114534ce9e
Stetsyuk Daniel <dancatmeow.daniel@gmail.com>
2026-09-05 20:47:30 +0800

    Version 2

--- Mazeproject/ContentView.swift
+++ Mazeproject/ContentView.swift
@@ -18,26 +18,106 @@ struct playerinfo:Hashable {
     var x : Int
     var y : Int
 }
-
+func walkanim(_ frame: Int) -> Int{
+    if frame == 1{
+        return 2
+    }else{
+        return 1
+    }
+}
+func cat( _ frame: Int,  _ x: Int, _ y: Int) -> some View {
+    if frame == 1{
+        Image("catwalk1")
+            .resizable()
+            .frame(width: 80, height: 70)
+            .position(x: CGFloat(x), y: CGFloat(y))
+    }else{
+        Image("catwalk2")
+            .resizable()
+            .frame(width: 100, height: 70)
+            .position(x: CGFloat(x), y: CGFloat(y))
+    }
+}
+//func catwalk(  _ direction: String, _ frame: Int,  _ x: Int, _ y: Int) -> some View {
+//    
+//        if direction == "north"{
+//            for i in 1...10{
+//            
+//            }
+//        }
+//    }
+//        
+//    
+//}
 struct ContentView: View {
     @State var walls = randomwall()
     @State var player = playerinfo(hp: 100, coin: 0, inventory: [], x: 0, y: 0)
+    @State var catFrame = 1
+    @State var catX = 200
+    @State var catY = 250
+    @State var catwalking: Bool = true
+    func catWalk(_ direction: String) async {
+        for _ in 1...10 {
+            catFrame = walkanim(catFrame)
+            if direction == "north" {
+                catY -= 15
+            } else if direction == "south" {
+                catY += 15
+            } else if direction == "east" {
+                catX += 15
+            } else if direction == "west" {
+                catX -= 15
+            }
+            try? await Task.sleep(nanoseconds: 50_000_000)
+            
+        }
+    }
+    func catWalktomid(_ direction: String) async {
+        if direction == "north" {
+            catY += 300
+        } else if direction == "south" {
+            catY -= 300
+        } else if direction == "east" {
+            catX -= 300
+        } else if direction == "west" {
+            catX += 300
+        }
+        for _ in 1...10 {
+            catFrame = walkanim(catFrame)
+            if direction == "north" {
+                catY -= 15
+            } else if direction == "south" {
+                catY += 15
+            } else if direction == "east" {
+                catX += 15
+            } else if direction == "west" {
+                catX -= 15
+            }
+            try? await Task.sleep(nanoseconds: 50_000_000)
+            
+        }
+    }
+
     var body: some View {
         VStack {
             HStack(spacing: 0) {
                 Button {
                     walls = randomwall()
-                } label: {
-                    Text("Coords: \(player.x), \(player.y), Coins: \(player.coin)")
-                        .padding(.horizontal, 10)
-                        .padding(.vertical, 5)
-                        .background(Color.gray.opacity(0.3))
-                        .clipShape(Capsule())
-                       
+                    } label: {
+                        Text("Coords: \(player.x), \(player.y), Coins: \(player.coin)")
+                            .padding(.horizontal, 10)
+                            .padding(.vertical, 5)
+                            .background(Color.gray.opacity(0.3))
+                            .clipShape(Capsule())
+                           
+                    }
                 }
                 
+                
             }
+            
             ZStack {
+                cat(catFrame,catX,catY)
                 Rectangle()
                     .frame(width: 404, height: 50)
                     .position(x: 200, y: 50)
@@ -73,8 +153,13 @@ struct ContentView: View {
                         .frame(width: 150, height: 50)
                         .position(x: 200, y: 50)
                     Button {
+                        Task{
+                            await catWalk("north")
+                            walls = randomwall()
+                            await catWalktomid("north")
                         player.y += 1
-                        walls = randomwall()
+                        
+                        }
                     } label: {
                         Text("↑")
                             .font(.largeTitle)
@@ -92,8 +177,13 @@ struct ContentView: View {
                         .frame(width: 50, height: 150)
                         .position(x: 0, y: 250)
                     Button {
+                        Task{
+                            await catWalk("west")
+                            walls = randomwall()
+                            await catWalktomid("west")
+                        }
                         player.x -= 1
-                        walls = randomwall()
+                        
                     } label: {
                         Text("↑")
                             .font(.largeTitle)
@@ -113,8 +203,14 @@ struct ContentView: View {
                         .frame(width: 50, height: 150)
                         .position(x: 400, y: 250)
                     Button {
+                        Task{
+                            await catWalk("east")
+                            walls = randomwall()
+                            await catWalktomid("east")
+                        }
                         player.x += 1
-                        walls = randomwall()
+                        
+                        
                     } label: {
                         Text("↑")
                             .font(.largeTitle)
@@ -134,8 +230,14 @@ struct ContentView: View {
                         .frame(width: 150, height: 50)
                         .position(x: 200, y: 450)
                     Button {
+                        Task{
+                            await catWalk("south")
+                            walls = randomwall()
+                            await catWalktomid("south")
+                            }
                         player.y -= 1
-                        walls = randomwall()
+                        
+                        
                     } label: {
                         Text("↑")
                             .font(.largeTitle)
@@ -159,7 +261,7 @@ struct ContentView: View {
             }
         }
     }
-}
+
 
 #Preview {
     ContentView()
