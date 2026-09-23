b8bc3e5b8bc5915ab4159d87c04528de39341b7b
Stetsyuk Daniel <dancatmeow.daniel@gmail.com>
2026-09-23 11:28:15 +0800

    lowk uhhh idk what i changed

---
+++ Mazeproject/Settingsview.swift
@@ -0,0 +1,18 @@
+//
+//  Settingsview.swift
+//  Mazeproject
+//
+//  Created by Daniel Stetsyuk on 14/9/26.
+//
+
+import SwiftUI
+
+struct Settingsview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Settingsview()
+}
